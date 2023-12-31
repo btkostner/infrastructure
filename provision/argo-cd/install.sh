c7fe87e132705189a6ed3e2d312d3212e6e46495
Blake Kostner <git@btkostner.io>
2023-12-31 14:22:03 -0700

    chore: move application sets away from argocd application

--- provision/argo-cd/install.sh
+++ provision/argo-cd/install.sh
@@ -5,13 +5,11 @@ set -e
 KUBE_CONTEXT="admin@btkostner"
 KUBE_NAMESPACE="argocd"
 
-cd "$(dirname "$0")"
-cd ../../cluster/core/argocd/argo-cd
-
 echo "=== Creating argocd namespace"
 
 kubectl --context "$KUBE_CONTEXT" create namespace "$KUBE_NAMESPACE" --dry-run=client -o yaml | kubectl --context "$KUBE_CONTEXT" apply -f -
 
 echo "=== Installing Argo CD"
 
+cd "$(dirname "$0")/../../cluster/core/argocd/argo-cd"
 kubectl --context "$KUBE_CONTEXT" kustomize . | kubectl --context "$KUBE_CONTEXT" apply -n "$KUBE_NAMESPACE" -f -
