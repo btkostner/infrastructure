851066103459f772ccece3a327156a1bb1a980f7
Blake Kostner <git@btkostner.io>
2024-01-01 19:57:56 -0700

    chore: setup new crds application

--- provision/core/install.sh
+++ provision/core/install.sh
@@ -4,6 +4,10 @@ set -e
 
 KUBE_CONTEXT="admin@btkostner"
 
+echo "=== Installing CRDs"
+
+kubectl kustomize "$(dirname "$0")/../../cluster/crds/" --enable-helm | kubectl --context "$KUBE_CONTEXT" apply -f -
+
 echo "=== Installing Core Resources"
 
 kubectl kustomize "$(dirname "$0")/../../cluster/core/" --enable-helm | kubectl --context "$KUBE_CONTEXT" apply -f -
